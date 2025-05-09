6ded7f5249795cd964fcc3afbfa7caefd7a5cc2d
Biagi Dorian <44123112+Dairop@users.noreply.github.com>
2025-05-09 10:03:04 +0200

    installer.sh plus versatile

--- installer.sh
+++ installer.sh
@@ -1,21 +1,53 @@
 #!/bin/bash
 
-set -e
-if conda info --envs | grep -q "^quchemenv"; then
-  conda remove -n quchemenv --all -y
+set -euo pipefail
+ENV_NAME="quchemenv"
+
+#Supprime env si existe deja
+if conda info --envs | grep -q "^$ENV_NAME"; then
+  echo "Suppression environnement existant: $ENV_NAME"
+  conda remove -n "$ENV_NAME" --all -y
 fi
-conda create -n quchemenv python=3.8.0 numpy=1.19.5 scipy=1.9.3 scikit-learn=0.23.2 -c conda-forge -y
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate quchemenv
-pip install requests==2.25.1 psutil==5.8.0 cclib==1.8.1 Pillow==8.1.2 pylatex==1.4.1
-conda install -c conda-forge openbabel -y
-conda install -c conda-forge mayavi -y
+
+#creation de l'environnement et priorite strict pour l'installation 
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+
+
+conda create -n "$ENV_NAME" \
+  python=3.8 \
+  numpy=1.21.6 \
+  scipy=1.9.3 \
+  scikit-learn=0.23.2 \
+  openbabel \
+  mayavi \
+  -c conda-forge -y
+
+#active environnement conda
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate "$ENV_NAME"
+
+
+pip install \
+  requests==2.25.1 \
+  psutil==5.8.0 \
+  cclib==1.8.1 \
+  Pillow==8.1.2 \
+  pylatex==1.4.1 \
+  cython==0.29.21
+
+#dependences (pour orbkit et mayavi)
 sudo apt update
 sudo apt install -y python3-dev cython3 libhdf5-dev gcc g++ git
+
+
+#clone d'orbkit
+cd "$(dirname "$0")"
 rm -rf orbkit
 git clone https://github.com/orbkit/orbkit.git
-cd orbkit/
-pip install cython==0.29.21
+cd orbkit
 pip install .
 cd ..
+
+
 python3 main.py examples/H2O_TD.log examples/H2O_OPT.log
