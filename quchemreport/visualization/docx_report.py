9611c2ba17d47870986e12e90e3b549f14dbd6c0
loic-berthelot <loic.berthelot49@gmail.com>
2025-05-20 11:38:52 +0200

    mise_en_page

--- quchemreport/visualization/docx_report.py
+++ quchemreport/visualization/docx_report.py
@@ -1,4 +1,5 @@
 from docx import Document
+from docx.enum.table import WD_TABLE_ALIGNMENT
 from docx.shared import Inches, Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 import os
@@ -8,16 +9,22 @@ from docx.oxml.ns import qn
 import numpy as np
 
 def add_section_title(doc, title_text):
+    section = doc.sections[0]
+    section.left_margin = Inches(0.6)
+    section.right_margin = Inches(0.6)
     p = doc.add_paragraph()
+    p.paragraph_format.left_indent = Pt(0)
+    p.paragraph_format.right_indent = Pt(0)
+    p.paragraph_format.space_before = Pt(0)
+    p.paragraph_format.space_after = Pt(0)
     run = p.add_run(title_text)
     run.bold = True
     run.font.size = Pt(14)
     run.font.color.rgb = RGBColor(255, 255, 255)
-    p_format = p.paragraph_format
-    p_format.space_before = Pt(0)
-    p_format.space_after = Pt(0)
     shading_elm = OxmlElement('w:shd')
-    shading_elm.set(qn('w:fill'), '009080')  # Vert canard
+    shading_elm.set(qn('w:val'), 'clear')
+    shading_elm.set(qn('w:color'), 'auto')
+    shading_elm.set(qn('w:fill'), '009080')
     p._element.get_or_add_pPr().append(shading_elm)
 
 def set_cell_border(cell, **kwargs):
@@ -74,12 +81,19 @@ def set_all_cell_borders(table, visible) :
 
 def create_table(doc, t):
     table = doc.add_table(rows=len(t), cols=len(t[0]))
+    table.alignment = WD_TABLE_ALIGNMENT.LEFT
+    table.allow_autofit = False
     table.style = 'Table Grid'
     for i in range(len(t)):
         row = table.rows[i]
         for j in range(len(t[i])):
             row.cells[j].text = str(t[i][j])
     set_all_cell_borders(table, False)
+    tbl_pr = table._element.xpath('.//w:tblPr')[0]
+    tbl_ind = OxmlElement('w:tblInd')
+    tbl_ind.set(qn('w:w'), str(int(600)))
+    tbl_ind.set(qn('w:type'), 'dxa')
+    tbl_pr.append(tbl_ind)
     return table
 
 
@@ -123,15 +137,14 @@ def json2docx(config, json_list, data, mode="clean"):
         if (len(data_ref["molecule"]["smi"]) < 80):
             t.append(["SMILES", data_ref["molecule"]["smi"]])
     table = create_table(doc, t)
-    table.columns[0].width = Pt(80);
-    table.columns[1].width = Pt(400);
+    table.columns[0].width = Pt(160);
+    table.columns[1].width = Pt(320);
     display_vertical_lines(table, False)
     doc.add_paragraph()
-    add_section_title(doc, "2. COMPUTATIONAL DETAILS")
 
 
     ### SECTION 2
-
+    add_section_title(doc, "2. COMPUTATIONAL DETAILS")
     t = []
     software = data_ref["comp_details"]["general"]["package"]
     try:
@@ -225,6 +238,149 @@ def json2docx(config, json_list, data, mode="clean"):
             except KeyError:
                 pass
         t.append([" ", " ", " "])
+    table = create_table(doc, t)
+    set_all_cell_borders(table, False)
+    display_vertical_lines(table)
+    set_vertical_line(table, 1, False)
+    doc.add_paragraph("\nJob type: Geometry optimization")
+    table.columns[0].width = Pt(200)
+    table.columns[1].width = Pt(200)
+    table.columns[2].width = Pt(80)
+
+
+    ### section 3 : results
+    t = []
+    add_section_title(doc, "3. RESULTS")
+    # Common results / wavefunction :
+    t.append(['Total molecular energy', "%.5f hartrees" %
+                       data_ref["results"]["wavefunction"]["total_molecular_energy"], " "])
+    homo_ind = data_ref["results"]["wavefunction"]["homo_indexes"]
+    MO_energies = data_ref["results"]["wavefunction"]["MO_energies"]
+    if len(homo_ind) == 2:
+        # Unrestricted calculation: two columns of MO energies
+        t.append(['Unrestricted calculation', 'Alpha spin MO',
+                           'Beta spin MO'])  # indices begin at 0, remove brackets
+        t.append(['HOMO number', homo_ind[0] + 1, homo_ind[1] + 1])
+        t.append(['LUMO+1 energies', "%.2f eV" % MO_energies[0][homo_ind[0] + 2],
+                           "%.2f eV" % MO_energies[1][homo_ind[1] + 2]])
+        t.append(['LUMO   energies', "%.2f eV" % MO_energies[0][homo_ind[0] + 1],
+                           "%.2f eV" % MO_energies[1][homo_ind[1] + 1]])
+        t.append(['HOMO   energies', "%.2f eV" % MO_energies[0][homo_ind[0]],
+                           "%.2f eV" % MO_energies[1][homo_ind[1]]])
+        t.append(['HOMO-1 energies', "%.2f eV" % MO_energies[0][homo_ind[0] - 1],
+                           "%.2f eV" % MO_energies[1][homo_ind[1] - 1]])
+    else:
+        t.append(['HOMO number', homo_ind[0] + 1, " "])
+        t.append(['LUMO+1 energies', "%.2f eV" % MO_energies[0][homo_ind[0] + 2], " "])
+        t.append(['LUMO   energies', "%.2f eV" % MO_energies[0][homo_ind[0] + 1], " "])
+        t.append(['HOMO   energies', "%.2f eV" % MO_energies[0][homo_ind[0]], " "])
+        t.append(['HOMO-1 energies', "%.2f eV" % MO_energies[0][homo_ind[0] - 1], " "])
+
+        # CDFT Indices  table only in full report
+    if report_type == 'full':
+        t.append([" ", " ", " "])
+        try:
+            t.append(['CDFT indices: Electron Affinity', "%.4f hartrees" % data_ref["results"]["wavefunction"]["A"],""])
+        except KeyError:
+            pass
+        try:
+            t.append(['CDFT indices: Ionisation Potential', "%.4f hartrees" % data_ref["results"]["wavefunction"]["I"], ""])
+        except KeyError:
+            pass
+        try:
+            t.append(['CDFT indices: Electronegativity', "%.4f hartrees" % data_ref["results"]["wavefunction"]["Khi"], ""])
+        except KeyError:
+            pass
+        try:
+            t.append(['CDFT indices: Hardness', "%.4f hartrees" % data_ref["results"]["wavefunction"]["Eta"], ""])
+        except KeyError:
+            pass
+        try:
+            t.append(['CDFT indices: Electrophilicity', "%.4f " % data_ref["results"]["wavefunction"]["Omega"], ""])
+        except KeyError:
+            pass
+        try:
+            t.append(['CDFT indices: Electron-flow', "%.4f e-" % data_ref["results"]["wavefunction"]["DeltaN"], ""])
+        except KeyError:
+            pass
+            # Specific calculations results:
+            OPT_res_print = False
+            for i, jsonfile in enumerate(json_list):
+                # OPT calculation results:
+                if ((job_types[i] == ['OPT']) or (job_types[i] == ['FREQ', 'OPT']) \
+                    or job_types[i] == ['FREQ', 'OPT', 'TD']) and (OPT_res_print == False):
+                    j = str(i + 1)
+                    OPT_res_print = True  # to prevent repetition from OPT and FREQ
+                    t.append([" ", " ", " "])
+                    t.append(["Geometry optimization specific results", " ", " "])
+                    t.append(['Converged nuclear repulsion energy',
+                                       "%.5f Hartrees" % json_list[i]["results"]["geometry"][
+                                           "nuclear_repulsion_energy_from_xyz"], " "])
+
+                # FREQ calculation results:
+                if job_types[i] == ['FREQ'] or job_types[i] == ['FREQ', 'OPT'] or job_types[i] == ['FREQ', 'OPT', 'TD']:
+                    k = 0
+                    j = str(k + 1)
+                    t.append([" ", " ", " "])
+                    t.append(["Frequency and Thermochemistry specific results", " ", " "])
+                    try:
+                        rtemper = json_list[i]["comp_details"]["freq"]["temperature"]
+                    except KeyError:
+                        rtemper = []
+                    # ND-arrays
+                    try:
+                        vibrational_int = np.array(json_list[i]["results"]["freq"]["vibrational_int"])
+                    except KeyError:
+                        vibrational_int = []
+                    try:
+                        vibrational_freq = np.array(json_list[i]["results"]["freq"]["vibrational_freq"])
+                    except KeyError:
+                        vibrational_freq = []
+
+                    if len(vibrational_int) == 0:
+                        vibrational_int = []
+                    else:
+                        # Print number of negative frequencies
+                        nb_negatives = np.sum(vibrational_freq < 0, axis=0)
+
+                    if (len(vibrational_int) != 0) and (rtemper != "N/A"):
+                        if "zero_point_energy" in json_list[i]["results"]["freq"]:
+                            t.append(['Sum of electronic and zero-point energy',
+                                               "%.5f Hartrees" % json_list[i]["results"]["freq"]["zero_point_energy"],
+                                               " "])
+                        if "electronic_thermal_energy" in json_list[i]["results"]["freq"]:
+                            t.append(["Sum of electronic and thermal energies at  %.2f K" % rtemper,
+                                               "%.5f Hartrees" % json_list[i]["results"]["freq"][
+                                                   "electronic_thermal_energy"], " "])
+                        if "enthalpy" in json_list[i]["results"]["freq"]:
+                            t.append(["Enthalpy at %.2f K" % rtemper,
+                                               "%.5f Hartrees" % json_list[i]["results"]["freq"]["enthalpy"], " "])
+                        if "free_energy" in json_list[i]["results"]["freq"]:
+                            t.append(["Gibbs free energy at %.2f K" % rtemper,
+                                               "%.5f Hartrees" % json_list[i]["results"]["freq"]["free_energy"], " "])
+                        if "entropy" in json_list[i]["results"]["freq"]:
+                            t.append(["Entropy at %.2f K" % rtemper,
+                                               "%.5f Hartrees" % json_list[i]["results"]["freq"]["entropy"], " "])
+            # End of the big common result table.
+
+            ## List of tables that are not job associated but dependent of data_ref.
+            # Population analysis tables and Fukui condensed values table only in full reports
+        if report_type == 'full':
+            # Mulliken partial charges table
+            try:
+                mulliken = data_ref["results"]["wavefunction"]["Mulliken_partial_charges"]
+            except KeyError:
+                mulliken = []
+            # test other population analysis
+            try:
+                hirsh = data_ref["results"]["wavefunction"]["Hirshfeld_partial_charges"]
+            except KeyError:
+                hirsh = []
+            try:
+                cm5 = data_ref["results"]["wavefunction"]["CM5_partial_charges"]
+            except KeyError:
+                cm5 = []
+
 
     table = create_table(doc, t)
     set_all_cell_borders(table, False)
@@ -234,4 +390,5 @@ def json2docx(config, json_list, data, mode="clean"):
     table.columns[0].width = Pt(200)
     table.columns[1].width = Pt(200)
     table.columns[2].width = Pt(80)
+
     doc.save("test.docx")
